7d314d5fa7fc46b14210dce75af8ca23858eec2b
Robert Jackson <robert.w.jackson@me.com>
2014-04-26 13:14:28 -0400

    Use broccoli-caching-writer.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var fs = require('fs');
 var path = require('path');
-var Writer = require('broccoli-writer');
+var CachingWriter = require('broccoli-caching-writer');
 var helpers = require('broccoli-kitchen-sink-helpers')
 
-Remover.prototype = Object.create(Writer.prototype);
+Remover.prototype = Object.create(CachingWriter.prototype);
 Remover.prototype.constructor = Remover;
 function Remover (inputTree, options) {
   if (!(this instanceof Remover)) return new Remover(inputTree, options);
@@ -17,20 +17,18 @@ Remover.prototype._removeFile = function (directory, source) {
   fs.unlinkSync(path.join(directory, source));
 };
 
-Remover.prototype.write = function (readTree, destDir) {
+Remover.prototype.updateCache = function (srcDir, destDir) {
   var self = this
 
-  return readTree(this.inputTree).then(function (srcDir) {
-    helpers.copyRecursivelySync(srcDir, destDir);
+  helpers.copyRecursivelySync(srcDir, destDir);
 
-    if (Array.isArray(self.files)) {
-      self.files.forEach(function(file) {
-        self._removeFile(destDir, file);
-      });
-    } else {
-      self._removeFile(destDir, self.srcFile);
-    }
-  })
+  if (Array.isArray(self.files)) {
+    self.files.forEach(function(file) {
+      self._removeFile(destDir, file);
+    });
+  } else {
+    self._removeFile(destDir, self.srcFile);
+  }
 };
 
 module.exports = Remover;
